ef26f34049a5bdb5ba0eac705378163391ceaa93
Shuban127893 <shubanbala25@gmail.com>
2024-12-14 13:40:13 -0500

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Shuban");
+        System.out.println("My favorite hobby is playing soccer");
+    } 
+}
